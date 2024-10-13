f70b15ffe759b650ef8c72e1cfad768dfd83a064
Casveira <lucastorres5@live.com>
2024-10-13 10:55:58 -0300

    C++ control flow

--- languages/cpp/helloworld.cpp
+++ languages/cpp/helloworld.cpp
@@ -1,6 +1,7 @@
 // #include header files: functions, classes, variables, etc.
 // <iostream> represents the default input/output (console)
 #include <iostream>
+#include <cmath>
 /* This allows the use of standart library (std) without
 std:: prefix. Ex: std::cout */
 using namespace std;
@@ -59,11 +60,75 @@ int main(){
     cout << "The phrase was: " << i << endl;
 
     /* Strings are objects and have methods. Since it is
-    not a type primitive it has to be included */ 
-    
+    not a type primitive it has to be #included */ 
     h[0] = 'J'; //
     cout << h << endl;
-    cout << "last typed letter is: " << h[h.length() - 1] << endl;
+    cout << "last typed letter is: " << h[h.length() - 1] << endl; //length is a string method
+
+    /* min and max are default?
+    <cmath> has sqrt, round, log support */
+    cout << "Min value of 5 and 10 is: " << min(5, 10) << endl;
+    cout << "Max value of 5 and 10 is: " << max(5, 10) << endl;
+    cout << "Square root of 64 " << sqrt(64) << endl;
+    cout << "Rounding of 2.6 " << round(2.6) << endl;
+    cout << "Log 2 " << log(2) << endl;
+
+    /* Control flow */
+    int age = 18;
+    int votingAge = 18;
+
+    if (age > votingAge){
+        cout << "Old enough to vote." << endl;
+    } else if (age == votingAge) {
+        cout << "Just enough old to vote." << endl;
+    } else {
+        cout << "Not old enough to vote." << endl;
+    }
+
+    /* Ternary operator*/
+    string oldEnough = (age > votingAge) ? "Ternary old" : "Ternary young";
+
+    /* Switch*/
+    int day = 4;
+
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    default:
+        cout << "Weekend";
+        break;
+    }
+
+    /* Loops */
+    int iteration = 1;
+    while (iteration <= 5){
+        cout << iteration;
+        iteration ++;
+    }
+
+    do {
+        iteration ++;
+        cout << iteration;
+    }
+    while (iteration <= 10);
+
+
+
+
 
 
     return 0;
