d9caa0620162fa6a4659e1faa729f567d98e8b5f
Casveira <lucastorres5@live.com>
2024-10-18 18:41:43 -0300

    Add sign & distance. Refactor magnitude and normalize

--- math/vector3D.py
+++ math/vector3D.py
@@ -7,8 +7,28 @@ class Vector3D:
         self.y = y
         self.z = z
     
-    def magnitude(self) -> float:
-        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+    def magnitude(self, vector:"Vector3D") -> float:
+        return sqrt(vector.x ** 2 + vector.y ** 2 + vector.z ** 2)
+    
+    def sign(self, vector:"Vector3D") -> "Vector3D":
+        signedVector = Vector3D()
+        if vector.x == 0:
+            signedVector.x = 0
+        else: 
+            signedVector.x = vector.x / abs(vector.x)
+        if vector.y == 0:
+            signedVector.y = 0
+        else: 
+            signedVector.y = vector.y / abs(vector.y)
+        if vector.z == 0:
+            signedVector.z = 0
+        else: 
+            signedVector.z = vector.z / abs(vector.z)
+        return signedVector
+       
+    def distance(self, fromVector:"Vector3D", toVector:"Vector3D") -> "Vector3D":
+        diffToVectFromVect = toVector - fromVector
+        return Vector3D().magnitude(diffToVectFromVect)
 
     def normalize(self) -> 'Vector3D':
         magnitude = self.magnitude()
@@ -24,7 +44,7 @@ class Vector3D:
                 firstVector3D.y * secondVector3D.y + 
                 firstVector3D.z * secondVector3D.z)
 
-    def angleBetweenTwoVertices(self, fVector:'Vector3D', sVector:'Vector3D'):
+    def angleBetweenTwoVectors(self, fVector:'Vector3D', sVector:'Vector3D'):
         magnitudeA = fVector.magnitude() 
         magnitudeB = sVector.magnitude() 
         cosTheta = clip(self.dotProduct(fVector, sVector) / (magnitudeA * magnitudeB), -1, 1)
@@ -36,9 +56,9 @@ class Vector3D:
                         self.z + otherVector3D.z)
 
     def __sub__(self, otherVector3D:'Vector3D'):
-        return Vector3D(self.x - otherVector3D.x,
-                        self.y - otherVector3D.y,
-                        self.z - otherVector3D.z)
+        return Vector3D(abs(self.x - otherVector3D.x),
+                        abs(self.y - otherVector3D.y),
+                        abs(self.z - otherVector3D.z))
 
     def __mul__(self, scalar:float|int) -> 'Vector3D':
         if not isinstance(scalar, (int, float)):
@@ -51,3 +71,9 @@ class Vector3D:
 
     def __repr__(self) -> str:
         return f"Vector3D {self.x}, {self.y}, {self.z}"
+    
+
+test2 = Vector3D(2, 4, 4)
+test3 = Vector3D(5, 4, 3)
+test3 = test3.distance(test2, test3)
+print(test3)
